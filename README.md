677f37c4cd78a3a17b40a8d68c4943f9036fe9aa
kristuff <kristuff.fr@outlook.com>
2020-05-06 23:04:26 +0200

    inital commit

--- README.md
+++ README.md
@@ -1,15 +1,8 @@
 # minikit
 
-            _         _  _     _  _
- _ __ ___  (_) _ __  (_)| | __(_)| |_
-| '_ ` _ \ | || '_ \ | || |/ /| || __|
-| | | | | || || | | || ||   < | || |_
-|_| |_| |_||_||_| |_||_||_|\_\|_| \__|
-  
-Awesome themes and components 
-(c) 2017-2020 Kristuff <contact@kristuff.fr>
-
 > Mini html5 css js toolkit for moderns browsers
 
 
+# Licence
+MIT
 
--- gulpfile.js
+++ gulpfile.js
@@ -174,4 +174,205 @@ gulp.task('build-css', function(log) {
 });
 
 
+
+
+/** 
+ * *****************************
+ * ********** minizik **********
+ * *****************************
+ */ 
+
+/* --- css --- */
+gulp.task('clean-dist-css-minizik', function() {
+    return del(['dist/css/minizik*', '!dist/css'], {force: true})
+});
+gulp.task('build-css-minizik', function () {
+    return gulp.src(['src/build/minizik.scss'])
+    .pipe(sass({ importer: globImporter() }).on('error', sass.logError))
+    .pipe(autoprefixer())    
+    .pipe(cleanCSS({compatibility: '*', format: 'beautify'}))
+    .pipe(gulp.dest('dist/css'));
+});
+gulp.task('min-css-minizik', function () {
+    return gulp.src('dist/css/minizik.css')
+    .pipe(cleanCSS({compatibility: '*'}))
+    .pipe(rename({extname: ".min.css"}))
+    .pipe(gulp.dest('dist/css'));
+});
+gulp.task('copy-minizik-css', function () {
+    return gulp.src([
+        'dist/css/minizik*.css',
+    ])
+    .pipe(gulp.dest('/home/www/music.kristuff.fr/myzik/www/assets/css'));
+});
+gulp.task('copy-minikit-css-to-minizik', function () {
+    return gulp.src([
+            'dist/css/minikit*.css', 
+            'dist/css/minikit*.min.css',
+    ]) 
+    .pipe(gulp.dest('/home/www/music.kristuff.fr/myzik/www/assets/css'))
+});
+
+/* --- js --- */
+gulp.task('clean-dist-js-minizik', function() {
+    return del(['dist/js/minizik*', '!dist/js'], {force: true})
+});
+gulp.task('merge-js-minizik-setup', function () {
+    return gulp.src([
+        '/home/www/music.kristuff.fr/myzik/app/view/js/core.js', 
+        '/home/www/music.kristuff.fr/myzik/app/view/js/setup/*.js',
+    ])
+    .pipe(concat('minizik.setup.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+gulp.task('merge-js-minizik-core', function () {
+    return gulp.src([
+        '/home/www/music.kristuff.fr/myzik/app/view/js/core.js', 
+        '/home/www/music.kristuff.fr/myzik/app/view/js/app/*.js',
+        '/home/www/music.kristuff.fr/myzik/app/view/js/media/*.js',
+    ])
+    .pipe(concat('minizik.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+gulp.task('merge-js-minizik-config', function () {
+    return gulp.src([
+        '/home/www/music.kristuff.fr/myzik/app/view/js/core.js', 
+        '/home/www/music.kristuff.fr/myzik/app/view/js/admin/*.js',
+    ])
+    .pipe(concat('minizik.config.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+gulp.task('min-js-minizik', function () {
+    return gulp.src('dist/js/minizik*.js')
+    .pipe(uglify().on('error', function(e){
+            console.log(e);
+     }))
+     .pipe(rename({extname: ".min.js"}))
+     .pipe(gulp.dest('dist/js'));
+});
+gulp.task('copy-minizik-js', function () {
+    return gulp.src([
+        'dist/js/minizik*.js', 
+    ])
+    .pipe(gulp.dest('/home/www/music.kristuff.fr/myzik/www/assets/js'));
+});
+gulp.task('copy-minikit-js-to-minizik', function () {
+     return  gulp.src([
+            'dist/js/minikit.js', 
+            'dist/js/minikit.min.js',
+    ])
+    .pipe(gulp.dest('/home/www/music.kristuff.fr/myzik/www/assets/js'))
+});
+
+// ------------------------------------
+gulp.task('build-minizik', function(log) {
+    runSequence(
+        ['clean-dist-css-minizik', 'clean-dist-js-minizik'],
+        ['build-css-minizik', 'merge-js-minizik-core', 'merge-js-minizik-setup', 'merge-js-minizik-config'],
+        ['min-css-minizik', 'min-js-minizik'],
+        ['copy-minizik-css', 'copy-minizik-js'],
+        ['copy-minikit-css-to-minizik', 'copy-minikit-js-to-minizik'],
+        log
+    );
+});
+
+/** 
+ * *****************************
+ * ******** minitoring *********
+ * *****************************
+ */ 
+
+/* --- css --- */
+gulp.task('clean-dist-css-minitoring', function() {
+    return del(['dist/css/minitoring*', '!dist/css'], {force: true})
+});
+
+gulp.task('build-css-minitoring', function () {
+    return gulp.src(['src/build/minitoring.scss'])
+    .pipe(sass({ importer: globImporter() }).on('error', sass.logError))
+    .pipe(autoprefixer())    
+    .pipe(cleanCSS({compatibility: '*', format: 'beautify'}))
+    .pipe(gulp.dest('dist/css'));
+});
+gulp.task('min-css-minitoring', function () {
+    return gulp.src('dist/css/minitoring.css')
+    .pipe(cleanCSS({compatibility: '*'}))
+    .pipe(rename({extname: ".min.css"}))
+    .pipe(gulp.dest('dist/css'));
+});
+gulp.task('copy-minitoring-css', function () {
+    return gulp.src([
+        'dist/css/minitoring*.css',
+    ])
+    .pipe(gulp.dest('/home/www/minitoring.kristuff.fr/minitoring/public/assets/css'));
+});
+gulp.task('copy-minikit-css-to-minitoring', function () {
+    return gulp.src([
+            'dist/css/minikit*.css', 
+            'dist/css/minikit*.min.css',
+    ]) 
+    .pipe(gulp.dest('/home/www/minitoring.kristuff.fr/minitoring/public/assets/css'))
+});
+
+/* --- js --- */
+gulp.task('clean-dist-js-minitoring', function() {
+    return del(['dist/js/minitoring*', '!dist/js'], {force: true})
+});
+gulp.task('merge-js-minitoring-setup', function () {
+    return gulp.src([
+        '/home/www/minitoring.kristuff.fr/minitoring/app/assets/js/core.js', 
+        '/home/www/minitoring.kristuff.fr/minitoring/app/assets/js/setup/*.js',
+    ])
+    .pipe(concat('minitoring.setup.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+gulp.task('merge-js-minitoring-core', function () {
+    return gulp.src([
+        '/home/www/minitoring.kristuff.fr/minitoring/app/assets/js/core.js', 
+        '/home/www/minitoring.kristuff.fr/minitoring/app/assets/js/app/*.js',
+        '/home/www/minitoring.kristuff.fr/minitoring/app/assets/js/system/*.js',
+    ])
+    .pipe(concat('minitoring.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+gulp.task('merge-js-minitoring-config', function () {
+    return gulp.src([
+        '/home/www/minitoring.kristuff.fr/minitoring/app/assets/js/core.js', 
+        '/home/www/minitoring.kristuff.fr/minitoring/app/assets/js/admin/*.js',
+    ])
+    .pipe(concat('minitoring.config.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+gulp.task('min-js-minitoring', function () {
+    return gulp.src('dist/js/minitoring*.js')
+    .pipe(uglify().on('error', function(e){
+            console.log(e);
+     }))
+     .pipe(rename({extname: ".min.js"}))
+     .pipe(gulp.dest('dist/js'));
+});
+gulp.task('copy-minitoring-js', function () {
+    return gulp.src([
+        'dist/js/minitoring*.js', 
+    ])
+    .pipe(gulp.dest('/home/www/minitoring.kristuff.fr/minitoring/public/assets/js'));
+});
+gulp.task('copy-minikit-js-to-minitoring', function () {
+    return  gulp.src([
+           'dist/js/minikit.js', 
+           'dist/js/minikit.min.js',
+   ])
+   .pipe(gulp.dest('/home/www/minitoring.kristuff.fr/minitoring/public/assets/js'))
+});
+
+// ------------------------------------
+gulp.task('build-minitoring', function(log) {
+    runSequence(
+        ['clean-dist-css-minitoring', 'clean-dist-js-minitoring'],
+        ['build-css-minitoring', 'merge-js-minitoring-core', 'merge-js-minitoring-setup', 'merge-js-minitoring-config'],
+        ['min-css-minitoring', 'min-js-minitoring'],
+        ['copy-minitoring-css', 'copy-minitoring-js'],
+        ['copy-minikit-css-to-minitoring', 'copy-minikit-js-to-minitoring'],
+        log
+    );
 });
